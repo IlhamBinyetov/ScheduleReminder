1b20b8f7b261feb51c31506852355e48decd7550
Ilham Binyetov <ilhamgb@code.edu.az>
2024-07-25 18:27:38 +0400

    Create new Model and add some packages

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ScheduleReminder.Models;
 
 namespace ScheduleReminder.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        public DbSet<Reminder> Reminders { get; set; }
     }
 }
