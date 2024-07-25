89b42cab9f010be0e66361619ff3f97930afb681
Ilham Binyetov <ilhamgb@code.edu.az>
2024-07-25 18:52:26 +0400

    Create IReminderRepository

---
+++ Services/IReminderRepository.cs
@@ -0,0 +1,13 @@
+﻿using ScheduleReminder.Models;
+
+namespace ScheduleReminder.Services
+{
+    public interface IReminderRepository
+    {
+        Task<Reminder> CreateReminder(Reminder reminder);
+        Task<IEnumerable<Reminder>> GetReminders();
+        Task<Reminder> GetReminder(int id);
+        Task UpdateReminder(Reminder reminder);
+        Task DeleteReminders(int[] ids);
+    }
+}
---
+++ Services/ReminderService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScheduleReminder.Data;
+using ScheduleReminder.Models;
+
+namespace ScheduleReminder.Services
+{
+    public class ReminderService : IReminderRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ReminderService(ApplicationDbContext dbContext)
+        {
+                _dbContext = dbContext;
+        }
+        public async Task<Reminder> CreateReminder(Reminder reminder)
+        {
+            _dbContext.Reminders.Add(reminder);
+            await _dbContext.SaveChangesAsync();
+            return reminder;
+        }
+
+        public Task DeleteReminders(int[] ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Reminder> GetReminder(int id)
+        {
+            return await _dbContext.Reminders.FindAsync(id);
+        }
+
+        public Task<IEnumerable<Reminder>> GetReminders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateReminder(Reminder reminder)
+        {
+            _dbContext.Entry(reminder).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+
+
+
+
+}
